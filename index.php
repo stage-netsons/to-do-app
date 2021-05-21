fd212376f361de88797d7d127e4b7cac5388c4f2
lorenzoleccese <tulliosamoiraghi@gmail.com>
2021-05-21 11:56:30 +0200

    calendar picker & regex

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
 session_start();
-if(isset($_POST['todo_text'])){
+$errore = "Add Elemento to TO-DO List";
+$errore_colore = "";
+if(isset($_POST['todo_text'], $_POST['todo_date'])){
   // TODO validare todo_text
   
-    $_SESSION['todo'][] = $_POST['todo_text'];
-    header("Location: /to-do-app/", true,301); 
-    die();
+    $pattern = '/^[A-Za-z0-9!\s\'!-]+$/';
+    $result_todo = preg_match($pattern, $_POST['todo_text']);
+    if($result_todo){
+      $_SESSION['todo'][] = $_POST['todo_text'];
+      $_SESSION['todo_date'][] = $_POST['todo_date'];
+      $_SESSION['todo_complete'] = array_combine($_SESSION['todo'], $_SESSION['todo_date']);
+      header("Location: /to-do-app/", true,301); 
+      echo "hello";
+      die();
+    }
+    else{
+      $errore = "caratteri speciali ammessi: ! ' -";
+      $errore_colore = "errore";
+    }
 }
+
 if(isset($_POST['check'])){
-    if(isset($_SESSION['todo'][$_POST['check']])){
-        $to_check = $_SESSION['todo'][$_POST['check']];
+    if(isset($_SESSION['todo_complete'][$_POST['check']])){
+        $to_check = $_SESSION['todo_complete'][$_POST['check']];
         $_SESSION['completed'][] = $to_check;
-        unset($_SESSION['todo'][$_POST['check']]);
+        unset($_SESSION['todo_complete'][$_POST['check']]);
     }
     header("Location: /to-do-app/", true,301);
     die();
@@ -32,6 +46,7 @@ if(isset($_POST['reset'])){
     <title>To-Do App - My First PHP App!</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
+    
   </head>
   <body>
     <div class="grid-container">
@@ -43,12 +58,22 @@ if(isset($_POST['reset'])){
       <div class="grid-x grid-padding-x">
         <div class="large-12 cell">
           <div class="callout">
-            <h3>My First PHP App</h3>
+            <h3>My Second PHP App</h3>
             <p>Simple and Fast <code>PHP</code> To-Do App in just 100 Lines of Code 😆!</p>
             <div class="large-12 cell">
                 <form action="" method="POST">
-                    <input type="text" name="todo_text" placeholder="Add Elemento to TO-DO List">
+                    <div style="display: flex;">
+                      <input type="text" name="todo_text"  class= "<?=$errore_colore?>" placeholder="<?=$errore?>">
+                      <input type="date" name="todo_date" value="2021-05-21" min="2021-05-21" max="2040-12-31">
+                    </div>
+                    
                     <input type="submit" value="Add" class="success button" style="width: 100%;">
+                    
+                            
+                            
+                    <?php
+
+                    ?>
                 </form>
             </div>
             <hr>
@@ -56,13 +81,13 @@ if(isset($_POST['reset'])){
               <div class="large-12 medium-12 cell">
                 <p><a href="#">To-Do:</a></p>
                 <?php
-                if(isset($_SESSION['todo'])){
+                if(isset($_SESSION['todo_complete'])){
                 ?>
                     <form action="" method="POST">
                     <?php
-                    foreach ($_SESSION['todo'] as $num => $todo) {
+                    foreach ($_SESSION['todo_complete'] as $num => $todo) {
                     ?>
-                        <input onclick="setTimeout(()=>{ return submit()}, 300);"  type="checkbox" name="check" value="<?=$num?>"> <?=$todo?>
+                        <input onclick="setTimeout(()=>{ return submit()}, 300);"  type="checkbox" name="check" value="<?=$num?>"> <?=$num?>  <?=$todo?>
                         <br>
                     <?php
                     }
