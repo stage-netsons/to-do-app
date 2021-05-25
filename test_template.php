dbdeb3b8ed18cfc140915153f76c6e7cfac1df0d
Mircha Emanuel D'Angelo <m.dangelo@netsons.com>
2021-05-25 15:08:45 +0200

    templating

---
+++ test_template.php
@@ -0,0 +1,37 @@
+<?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+//caricamento engine
+$loader = new FilesystemLoader(__DIR__ . '/templates/');
+$twig = new Environment($loader, [
+    //    'cache' => __DIR__ . '/templates_c', //cache template compilati
+    'cache' => false,
+]);
+
+//caricamento template
+$template = $twig->load('index.html.twig');
+
+//LOGICA
+$users = [
+    ['name' => 'Chiara Cerrone', 'email' => 'chiara@cerrone.it'],
+    ['name' => 'Lorenzo Leccese', 'email' => 'lorenzo@leccese.it'],
+];
+
+
+//variabili template
+$vars = [
+    'page_title'    => 'Demo Twig - Stage Netsons',
+    'users'         => $users,
+    'username'      => 'chiara',
+    'logged_in'     => true, //false
+    'todo_complete' => [
+        ['impegno' => 'git pull', 'data' => '20/02/2021']
+    ]
+];
+
+echo $template->render($vars);
+
